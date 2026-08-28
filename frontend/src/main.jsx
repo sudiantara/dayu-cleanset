70f029c2eb13748b2285b0a3ad368e04136c85e2
IDA BAGUS MADE SUDIANTARA <122762489+sudiantara@users.noreply.github.com>
2026-08-28 19:07:48 +0800

    style: load global responsive layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./Responsive.css";
 import App from "./App.jsx";
 import AuthGate from "./AuthGate.jsx";
 import { installOrderDetailEnhancer } from "./orderDetailEnhancer.jsx";
